2e52444aacef1a6d7a7769d607b4e92de77ef97c
shou-317 <shou6130@gmail.com>
2025-07-02 15:57:49 +0900

    refactor: remove unnecessary locale handling from books views and controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url(locale: params[:locale]), notice: t('flash.books.delete') }
+      format.html { redirect_to books_url, notice: t('flash.books.delete') }
       format.json { head :no_content }
     end
   end
