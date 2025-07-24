0ee72cd01abcebcb211568b3f93ffbc1f08d629a
horishota6566 <shou6130@gmail.com>
2025-07-24 14:41:30 +0900

    ユーザー登録で許可するパラメータを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    keys = [:postal_code, :address, :introduction]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
 end
