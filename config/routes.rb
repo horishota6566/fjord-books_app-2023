4a5e1fde93be8331e4a76bc066127f677b6a58dd
horishota6566 <shou6130@gmail.com>
2025-08-02 09:40:46 +0900

    refactor(routes): ルーティングの順序を整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :reports
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: 'books#index'
-  resources :books
   resources :users, only: %i(index show)
+  resources :books
+  resources :reports
+  resources :comments
 end
