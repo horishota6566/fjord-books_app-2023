0d30c1e555d81d360003ab7239c0369003e1f3c4
horishota6566 <shou6130@gmail.com>
2025-08-02 09:42:48 +0900

    refactor(routes): commentsのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: 'books#index'
   resources :users, only: %i(index show)
-  resources :books
-  resources :reports
-  resources :comments
+  resources :books do
+    resources :comments, only: [:create]
+  end
+  resources :reports do
+    resources :comments, only: [:create]
+  end
+  resources :comments, only: %i(edit update destroy)
 end
