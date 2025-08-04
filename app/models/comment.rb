2e8402d6008a216d385358668c8b2f933a77f834
horishota6566 <shou6130@gmail.com>
2025-08-04 18:03:02 +0900

    feat(comment): 作成日時の降順で取得するように制御

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+  default_scope -> { order(created_at: :desc) }
   validates :content, presence: true
 end
