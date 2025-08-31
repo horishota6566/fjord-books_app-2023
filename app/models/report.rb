762610f78e99dae4396e0b2f9b00421ef4d6ce74
horishota6566 <shou6130@gmail.com>
2025-08-31 18:40:39 +0900

    feat(report): mentionとの関連を追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,14 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name:  'Mention',
+                                  foreign_key: 'mentioning_id',
+                                  dependent:   :destroy
+  has_many :passive_mentions, class_name:  'Mention',
+                                  foreign_key: 'mentioned_id',
+                                  dependent:   :destroy
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioned
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioning
 
   validates :title, presence: true
   validates :content, presence: true
