4314da4c21aa896a265fc1afdf40bed41fbbeef1
horishota6566 <shou6130@gmail.com>
2025-08-31 18:44:11 +0900

    feat(report): 作成・更新時に言及を再構築する処理を追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -15,11 +15,25 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_commit :rebuild_mentions!, on: %i[create update]
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  private
+  def extract_report_ids_from(text)
+    ids = text.scan(%r{http://localhost:3000/reports/(\d+)})
+    ids.flatten.map(&:to_i).uniq.without(id)
+  end
+
+  def rebuild_mentions!
+    ids = extract_report_ids_from(content)
+    active_mentions.delete_all
+    mentioning_reports << Report.where(id: ids)
   end
 end
