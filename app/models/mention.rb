ddfbb6e22e2bf6619ed3aec20643d37d96e85371
horishota6566 <shou6130@gmail.com>
2025-09-01 17:02:28 +0900

    refactor: rubocopでコードを整形

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Mention < ApplicationRecord
-  belongs_to :mentioning, class_name: 'Report'
-  belongs_to :mentioned, class_name: 'Report'
+  belongs_to :mentioning, class_name: 'Report', inverse_of: :active_mentions
+  belongs_to :mentioned, class_name: 'Report', inverse_of: :passive_mentions
   validates :mentioning_id, presence: true
   validates :mentioned_id, presence: true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,12 +3,16 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :active_mentions, class_name:  'Mention',
-                                  foreign_key: 'mentioning_id',
-                                  dependent:   :destroy
-  has_many :passive_mentions, class_name:  'Mention',
-                                   foreign_key: 'mentioned_id',
-                                   dependent:   :destroy
+  has_many :active_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioning_id',
+           dependent: :destroy,
+           inverse_of: :mentioning
+  has_many :passive_mentions,
+           class_name: 'Mention',
+           foreign_key: 'mentioned_id',
+           dependent: :destroy,
+           inverse_of: :mentioned
   has_many :mentioning_reports, through: :active_mentions, source: :mentioned
   has_many :mentioned_reports, through: :passive_mentions, source: :mentioning
 
@@ -26,6 +30,7 @@ class Report < ApplicationRecord
   end
 
   private
+
   def extract_report_ids_from(text)
     ids = text.scan(%r{http://localhost:3000/reports/(\d+)})
     ids.flatten.map(&:to_i).uniq.without(id)
