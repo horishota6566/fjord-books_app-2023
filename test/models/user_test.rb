b5a4ef683b02a0dc7d1f8c415d7afab54f9aebc6
horishota6566 <shou6130@gmail.com>
2025-10-31 12:18:35 +0900

    test(user): モデルテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return name when name is present' do
+    user = users(:alice)
+    assert_equal 'Alice', user.name_or_email
+  end
+
+  test 'should return email when name is blank' do
+    user = users(:carol)
+    assert_equal 'carol@example.com', user.name_or_email
+  end
 end
